54e9fd109ac7375bad4aa8c3f52e224923082904
AartiBhalekar <90775695+AartiBhalekar@users.noreply.github.com>
2021-10-04 09:34:24 +0530

    Add files via upload

---
+++ Day11/Que 1/boot-security-custom-login-page/src/main/java/com/javainuse/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.javainuse.service;
+
+import java.util.List;
+
+import com.javainuse.model.Employee;
+
+public interface EmployeeService {
+	void insertEmployee(Employee emp);
+	void insertEmployees(List<Employee> employees);
+	List<Employee> getAllEmployees();
+	void getEmployeeById(String empid);
+	void deleteEmployee(int eid);
+}
